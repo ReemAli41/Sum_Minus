db45b929ed5056d9b1b3977c040f35d536e953ce
ReemAli41 <reem.ali.abdelrahman@gmail.com>
2025-05-13 13:20:53 +0300

    Update Controller and Services To Send Numbers and Route From Services

--- Controllers/OperationController.cs
+++ Controllers/OperationController.cs
@@ -1,42 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Sum_Minus.Services;
-using Sum_Minus.Entities;  
+using Sum_Minus.Entities;
 
 namespace Sum_Minus.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class OperationController : ControllerBase
     {
         private readonly IOperationService _operationService;
+        private readonly IApiCallService _apiCallService;
 
-        public OperationController(IOperationService operationService)
+        public OperationController(IOperationService operationService, IApiCallService apiCallService)
         {
             _operationService = operationService;
+            _apiCallService = apiCallService;
         }
 
         [HttpPost("sum")]
         public IActionResult Sum([FromBody] OperationRequest request)
         {
-            if (request == null)
-            {
-                return BadRequest("Invalid data.");
-            }
-
-            var result = _operationService.Sum(request.Number1, request.Number2);
-            return Ok(new { result });
+            var result = _operationService.Sum(request);
+            return StatusCode(result.StatusCode, result);
         }
 
         [HttpPost("minus")]
         public IActionResult Minus([FromBody] OperationRequest request)
         {
-            if (request == null)
-            {
-                return BadRequest("Invalid data.");
-            }
+            var result = _operationService.Minus(request);
+            return StatusCode(result.StatusCode, result);
+        }
 
-            var result = _operationService.Minus(request.Number1, request.Number2);
-            return Ok(new { result });
+        //For testing
+        [HttpGet("call-api")]
+        public async Task<IActionResult> CallExternalApi()
+        {
+            var result = await _apiCallService.CallApiAsync();
+            return StatusCode(result.StatusCode, result);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IOperationService, OperationService>();
+builder.Services.AddHttpClient<IApiCallService, ApiCallService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,3 +27,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
